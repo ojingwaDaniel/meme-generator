5260960b88b9b8a700313fe5fe21deeb1c06639b
ojingwaDaniel <ojingwadanny@gmail.com>
2024-01-26 03:54:46 +0100

    added a meme component

--- src/components/meme.jsx
+++ src/components/meme.jsx
@@ -1,17 +1,12 @@
 export default function Meme() {
   return (
-    <div className="meme">
-      <div className="input-form">
-        <form action="" className="input-form-1">
-          <label htmlFor="">Top text</label>
-          <input type="text" name="" id="" placeholder=" type in a text..." />
-        </form>
-        <form action="">
-          <label htmlFor="">Bottom text</label>
-          <input type="text" name="" id="" placeholder="type a text..." />
-        </form>
-      </div>
-      <button type="button">Get a new meme image 🖼</button>
-    </div>
+    <main>
+      <form action="" className="meme-form">
+        <input type="text" name="" id="" placeholder=" Top Text..." />
+        <input type="text" name="" id="" placeholder=" Botton Text..." />
+        <button type="button">Get a new meme image 🖼</button>
+      </form>
+    </main>
   );
 }
+
