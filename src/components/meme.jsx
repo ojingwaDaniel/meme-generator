6daea9b31c0ec1e4eb75103cea9c3e0a2e865c28
ojingwaDaniel <ojingwadanny@gmail.com>
2024-01-27 02:46:20 +0100

    added a meme component

--- src/components/meme.jsx
+++ src/components/meme.jsx
@@ -1,11 +1,19 @@
+import memeData from "../memeData";
 export default function Meme() {
+    function getImageUrl() {
+        const memeArray = memeData.data.memes
+        const randomNumber = Math.floor(Math.random() * memeArray.length + 1)
+        const url = memeArray[randomNumber].url
+        console.log(url)
+    }
+        
   return (
     <main>
-      <form action="" className="meme-form">
-        <input type="text" name="" id="" placeholder=" Top Text..." />
-        <input type="text" name="" id="" placeholder=" Botton Text..." />
-        <button type="button">Get a new meme image 🖼</button>
-      </form>
+      <div  className="meme-form">
+        {/* <input type="text" name="" id="" placeholder=" Top Text..." />
+        <input type="text" name="" id="" placeholder=" Botton Text..." /> */}
+        <button type="button" onClick={getImageUrl}>Get a new meme image 🖼</button>
+      </div>
     </main>
   );
 }
