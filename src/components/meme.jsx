3530ba7f9f5f0733e5783e30d6fbcc5621ac3e5e
ojingwaDaniel <ojingwadanny@gmail.com>
2024-01-29 00:08:30 +0100

    added a meme component

--- src/components/meme.jsx
+++ src/components/meme.jsx
@@ -17,4 +17,14 @@ export default function Meme() {
     </main>
   );
 }
-
+// state in react
+ const result = React.useState("Yes");
+ console.log(result);
+ return (
+   <div className="state">
+     <h1 className="state--title">Is state important to know?</h1>
+     <div className="state--value">
+       <h1>{result[0]}</h1>
+     </div>
+   </div>
+ );
